a582ad6ffcda20c68e7278de281faed4662c9e79
William Meighan <williamdmeighan@gmail.com>
2020-03-03 18:25:16 -0700

    create discounts controller and view

---
+++ app/controllers/merchant/discounts_controller.rb
@@ -0,0 +1,53 @@
+class Merchant::DiscountsController < Merchant::BaseController
+
+  def index
+    @discounts = Discount.all
+  end
+
+  def show
+    @discount = Discount.find(params[:discount_id])
+  end
+
+  def new
+  end
+
+  def create
+    merchant = current_user.merchant
+    discount = merchant.discounts.new(discount_params)
+    if discount.save
+      flash[:notice] = "Your discount has been added."
+      redirect_to "/merchant/discounts"
+    else
+      flash[:notice] = @discount.errors.full_messages.to_sentence
+      redirect_to "/merchant/discounts/new"
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    merchant = current_user.merchant
+    discount = Discount.find(params[:discount_id])
+    if discount.update(discount_params)
+      flash[:notice] = "Your discount changes have been saved."
+      redirect_to "/merchant/discounts/#{discount.id}"
+    else
+      flash[:notice] = "Your discount changes have not been saved."
+      redirect_to "/merchant/discounts/#{discount.id}/edit"
+    end
+  end
+
+  def destroy
+    discount = Discount.find(params[:discount_id])
+    discount.destroy
+    redirect_to "/merchant/discounts"
+  end
+
+  private
+
+    def discount_params
+      params.permit(:description, :item_threshold, :percent_discount)
+    end
+
+end
